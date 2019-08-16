ae190e6f4420bb0c7e70078996756b166cd4c6b8
Gustav Behm <gustav@upvest.co>
2019-08-16 15:43:30 +0200

    Refactor into a script based entrypoint to the minimizer

--- bin/minimize.sh
+++ bin/minimize.sh
@@ -2,12 +2,16 @@
 
 set -o nounset -o pipefail -o errexit
 
+SCRIPTS_DIR=$(readlink -f "$0" | xargs dirname)
 DOCKER=${DOCKER-docker}
-FROM=
-FILTER=
-while getopts "f:v:" OPT; do
+DOCKER_FILE=${DOCKER_FILE-Dockerfile}
+DOCKER_CONTEXT=${DOCKER_CONTEXT-.}
+FILTER=${FILTER-.dockerinclude}
+OUTPUT=${OUTPUT-.docker-image}
+while getopts "f:c:v:" OPT; do
     case $OPT in
-        f) FROM=$OPTARG ;;
+        f) DOCKER_FILE=$OPTARG ;;
+        c) DOCKER_CONTEXT=$OPTARG ;;
         v) FILTER=$OPTARG ;;
         \?) echo "Invalid option: -$OPTARG" >&2; exit 2 ;;
     esac
@@ -16,6 +20,10 @@ shift $((OPTIND-1))
 
 TMP=$(mktemp -d)
 trap "rm -rf $TMP" EXIT
+
+$DOCKER build --iidfile="$TMP/base.image" -f "$DOCKER_FILE" \
+    "$DOCKER_CONTEXT" >&2
+FROM=$(cat "$TMP/base.image")
 
 ENTRYPOINT=$($DOCKER inspect "$FROM" | jq --compact-output ".[0].Config.Entrypoint")
 WORKDIR=$($DOCKER inspect "$FROM" | jq --raw-output ".[0].Config.WorkingDir")
@@ -31,4 +39,4 @@ changes() {
 "$SCRIPTS_DIR/export-image.sh" "$FROM" \
     | "$SCRIPTS_DIR/filter-tarball.sh" -v "$FILTER" \
     | xargs -0 --arg-file=<(changes | sed 's/^/--change=/' | tr '\n' '\0') \
-        $DOCKER import -
+        $DOCKER import - > "$OUTPUT"
--- bin/strace-docker.sh
+++ bin/strace-docker.sh
@@ -6,23 +6,33 @@ DOCKER=${DOCKER-docker}
 DOCKER_RUN_OPTS=${DOCKER_RUN_OPTS-}
 TRIGGER_CMD=${TRIGGER_CMD-}
 WITH_RUNNING_CONTAINER=${WITH_RUNNING_CONTAINER-1}
-TRACE_FILE_IN_CONTAINER=${TRACE_FILE_IN_CONTAINER-/tmp/trace}
-while getopts "fd:i:p:t:rR" OPT; do
+TRACE_DIR_IN_CONTAINER=${TRACE_DIR_IN_CONTAINER-/tmp/traces}
+DOCKER_FILE=${DOCKER_FILE-Dockerfile}
+DOCKER_CONTEXT=${DOCKER_CONTEXT-.}
+OUTPUT=${OUTPUT-.dockerinclude}
+while getopts "f:c:d:i:p:t:rRo:-" OPT; do
     case $OPT in
-        d) DOCKER_OPTS="$DOCKER_OPTS $OPTARG" ;;
-        s) STRACE_OPTS="$STRACE_OPTS $OPTARG" ;;
+        f) DOCKER_FILE=$OPTARG ;;
+        c) DOCKER_CONTEXT=$OPTARG ;;
+        d) DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS $OPTARG" ;;
         i) INSTALL_STRACE_CMD="$OPTARG" ;;
         p) PACKAGE_MANAGER="$OPTARG" ;;
         t) TRIGGER_CMD="$OPTARG" ;;
         r) WITH_RUNNING_CONTAINER=1 ;;
         R) WITH_RUNNING_CONTAINER=0 ;;
+        o) OUTPUT=$OPTARG ;;
+        -) break ;;
         \?) echo "Invalid option: -$OPTARG" >&2; exit 2 ;;
     esac
 done
 shift $((OPTIND-1))
 
-INPUT=$1
-shift 1
+TMP=$(mktemp -d)
+trap "rm -rf $TMP" EXIT
+
+$DOCKER build --iidfile="$TMP/base.image" -f "$DOCKER_FILE" \
+    "$DOCKER_CONTEXT" >&2
+INPUT=$(cat "$TMP/base.image")
 
 if [[ ! -v INSTALL_STRACE_CMD ]]; then
     case ${PACKAGE_MANAGER-} in
@@ -31,9 +41,6 @@ if [[ ! -v INSTALL_STRACE_CMD ]]; then
         *) echo "PACKAGE_MANAGER variable not set" >&2; exit 2 ;;
     esac
 fi
-
-TMP=$(mktemp -d)
-trap "rm -rf $TMP" EXIT
 
 cat <<EOF > $TMP/Dockerfile
 FROM $INPUT
@@ -45,7 +52,7 @@ $DOCKER build --iidfile="$TMP/extended.image" "$TMP" >&2
 EXEC=$($DOCKER inspect "$INPUT" | jq -r ".[0].Config.Entrypoint[0]")
 EXEC=$($DOCKER run --rm \
     --entrypoint="/bin/sh" \
-    $(<"$TMP/extended.image") \
+    "$(<"$TMP/extended.image")" \
     -c 'which "'$EXEC'"'
 )
 
@@ -58,7 +65,7 @@ INTERPRETER=$($DOCKER run --rm \
 )
 
 # prepare to run strace
-STRACE='["strace", "-qq", "-z", "-D", "-o", "'$TRACE_FILE_IN_CONTAINER'", "-ff", "-f", "-e", "%file", "'$INTERPRETER'", "'$EXEC'"]'
+STRACE='["strace", "-qq", "-z", "-D", "-o", "'"$TRACE_DIR_IN_CONTAINER"'/trace", "-ff", "-f", "-e", "file", "'$INTERPRETER'", "'$EXEC'"]'
 
 ENTRYPOINT=$($DOCKER inspect "$INPUT" \
     | jq --compact-output "[$STRACE, .[0].Config.Entrypoint[1:]] | flatten"
@@ -70,14 +77,15 @@ EOF
 
 $DOCKER build --iidfile="$TMP/extended.image" "$TMP" >&2
 
-TRACE_OUTPUT=$TMP/trace
+TRACE_OUTPUT=$TMP/traces
+mkdir -p "$TRACE_OUTPUT"
 
 if [ -z "$TRIGGER_CMD" ]; then
     set +o errexit
     $DOCKER run --rm --cap-add=SYS_PTRACE \
-        --volume="$(dirname "$TRACE_OUTPUT"):$(dirname "$TRACE_FILE_IN_CONTAINER")" \
+        --volume="$TRACE_OUTPUT:$TRACE_DIR_IN_CONTAINER" \
         $DOCKER_RUN_OPTS \
-        $(<"$TMP/extended.image") $@ >&2
+        "$(<"$TMP/extended.image")" $@ >&2
     EXIT=$?
     set -o errexit
 else
@@ -90,26 +98,51 @@ else
 
     if [ "$WITH_RUNNING_CONTAINER" = "1" ]; then
         $DOCKER run --rm --cap-add=SYS_PTRACE --detach \
-            --volume="$(dirname "$TRACE_OUTPUT"):$(dirname "$TRACE_FILE_IN_CONTAINER")" \
+            --volume="$TRACE_OUTPUT:$TRACE_DIR_IN_CONTAINER" \
             --cidfile="$TMP/container" \
             $DOCKER_RUN_OPTS \
-            $(<"$TMP/extended.image") $@ >/dev/null
+            "$(<"$TMP/extended.image")" $@ >&2
 
-        DOCKER_IMAGE=$(<"$TMP/extended.image") \
-            DOCKER_CONTAINER=$(<"$TMP/container") \
-            TRACE_FILE_IN_CONTAINER="$TRACE_FILE_IN_CONTAINER" \
-            TRACE_OUTPUT="$TRACE_OUTPUT" \
+        DOCKER_CONTAINER=$(<"$TMP/container") \
             run_trigger
 
-        $DOCKER inspect $(<"$TMP/container") >/dev/null && $DOCKER stop $(<"$TMP/container") >/dev/null
+        $DOCKER inspect "$(<"$TMP/container")" &>/dev/null \
+            && $DOCKER stop "$(<"$TMP/container")" >/dev/null
     else
-        DOCKER_IMAGE=$(<"$TMP/extended.image") \
+        DOCKER_IMAGE="$(<"$TMP/extended.image")" \
             TRACE_OUTPUT="$TRACE_OUTPUT" \
-            TRACE_FILE_IN_CONTAINER="$TRACE_FILE_IN_CONTAINER" \
+            TRACE_DIR_IN_CONTAINER="$TRACE_DIR_IN_CONTAINER" \
             run_trigger
     fi
 fi
 
-cat "$TRACE_OUTPUT".*
+cat "$TRACE_OUTPUT/trace".* \
+    | grep -v "^--- SIG.*---$" \
+    | grep -v "<unfinished ...>" \
+    | while read -r L; do
+    SYSCALL=$(sed 's/^\(\w\+\)(.*$/\1/' <<< "$L")
+    ARGS=$(sed 's/^\w\+(\(.*\)$/\1/' <<< "$L")
+
+    grep -cq "stat$" <<< "$SYSCALL" && grep -cq "S_IFDIR" <<< "$ARGS" && continue
+
+    case "$SYSCALL" in
+        execve|open|access|readlink|stat|lstat) # first argument
+            FN=$(sed 's/^"\([^"]*\)".*$/\1/' <<< "$ARGS") ;;
+        openat) # second argument
+            FN=$(sed 's/^[^,]*,\s\+"\([^"]*\)".*$/\1/' <<< "$ARGS") ;;
+        getcwd|mkdir|statfs|chown|unlink|rename|chdir) # ignore
+            continue;;
+        *) echo 1>&2 "unhandled syscall $SYSCALL"; exit 2;;
+    esac
+
+    grep -cq '^/dev' <<< "$FN" && continue
+    grep -cq '^/sys' <<< "$FN" && continue
+    grep -cq '^/proc' <<< "$FN" && continue
+    grep -cq '^/tmp' <<< "$FN" && continue
+
+    grep -cq "__pycache__" <<< "$FN" && continue
+
+    echo "f	$FN"
+done | sort -u > "$OUTPUT"
 
 exit $EXIT
